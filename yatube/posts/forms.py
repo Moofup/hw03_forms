616f5f1c6fbe8c7a82d5d5c1c8f06479cf6c44b9
Бакушин Михаил <moofus@inbox.ru>
2022-01-28 13:09:11 +0300

    Questioned label and help_text

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -4,6 +4,17 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+
     class Meta:
         model = Post
         fields = ('text', 'group')
+
+        label = {
+            'text': 'Текст поста',
+            'group': 'Группа',
+        }
+
+        help_text = {
+            'text': 'Текст поста',
+            'group': 'Группа,к которой будет относиться пост',
+        }
